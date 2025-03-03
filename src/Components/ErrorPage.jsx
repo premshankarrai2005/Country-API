fc74fbc707ebe533f759da414e65bcc935356308
Prem Shankar Rai <premshankarrai2005@gmail.com>
2025-03-03 17:21:56 +0530

    header position fixed

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-
+import { DarkModeContext } from '../contexts/ThemecontextdarkMode';
+import './style.css'
+import errorimg from '../assets/404-error-with-landscape-concept-illustration_114360-7898-removebg-preview.png'
 function ErrorPage() {
+    const [isDark] = useContext(DarkModeContext)
     return (
-        <div className="error-container">
-            <img
-                src="https://img.freepik.com/free-vector/error-404-concept-illustration_114360-1811.jpg"
+        <main className={`error-container ${isDark ? 'dark' : ''}`}>
+            < img
+                src={errorimg}
                 alt="Error"
                 className="error-image"
             />
             <h1 className="error-title">Oops! Page Not Found</h1>
             <p className="error-message">The page you are looking for does not exist.</p>
             <Link to="/" className="error-button">Go Back Home</Link>
-        </div >
+        </main >
     );
 }
 
